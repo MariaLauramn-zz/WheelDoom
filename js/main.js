bbcf2ba3a7f5eb63a3785ecb4cf32a19a2cd0684
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-04-30 15:31:45 +0200

    main/indexofwinner/splice

--- js/main.js
+++ js/main.js
@@ -1,25 +1,12 @@
 const button = document.getElementById("spin");
 const resultext = document.getElementById("result");
-let names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
+const names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
 function selectName(){
     let winner = names[Math.floor(Math.random() * names.length)];
-    document.getElementById("spin").innerHTML="Spin";
-    message = winner;
-    resultext.innerHTML = message;
+    resultext.innerHTML = winner;
+    let indexOfWinner = names.indexOf(winner);
+    let deleteName = names.splice(indexOfWinner,1);
 }
 button.addEventListener("click",selectName)
-let spinner = document.getElementById("spin");
 
-
-function noRepetida(winner) {
-
-
-    function nameSelector() {
-        spinner = arr [Math.floor(Math.random() * arr.length)];
-        message = "La seleccionada es:  ";
-
-    }
-
-    document.getElementById("spinner").innerHTML = message;
-}
