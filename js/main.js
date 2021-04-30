538f618afbf52ee39ac69da7c964dc491bc2b24e
Marialauramn16 <hikco25@gmail.com>
2021-04-30 10:24:48 +0200

    prueba seleccion

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let arr = ['Pepi', 'Clau','Daniela T','Maria Laura', 'Amanda', 'Alisa','Ana G','Ana L', 'Angie', 'Daniela R' , 'Eliana',
     'Flo', 'Gaby', 'Leo', 'Inga', 'Marta', 'Nicole', 'Olga', 'Yeraldine', 'Ranju', 'Rosangely','Sonia', 'Taiza', 'Yasmin'];
 
-const spinner = document.getElementById("spin");
+let spinner = document.getElementById("spin");
 
     function chooseName(){
         console.log(arr[Math.floor(Math.random() * arr.length)]);
