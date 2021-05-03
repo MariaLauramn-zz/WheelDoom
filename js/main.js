88d30014e50f70c3e33468f747307e5ad03742db
Marialauramn16 <hikco25@gmail.com>
2021-05-03 11:01:46 +0200

    agregando lista

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const button = document.getElementById("spin");
 const resultext = document.getElementById("result");
+let group = document.getElementById("lista");
 const names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
 function selectName(){
@@ -8,12 +9,11 @@ function selectName(){
     let indexOfWinner = names.indexOf(winner);
     let deleteName = names.splice(indexOfWinner,1);
 }
-button.addEventListener("click",selectName)
 
-let group = document.getElementById("lista");
 function showList(){
     let people = names.slice();
     group.innerHTML = people;
+    let deleteName = names.splice(group,1);
 }
+button.addEventListener("click",selectName,showList())
 
-group.addEventListener("click",showList)
