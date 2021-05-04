493850d1022e6788ef3aa22a14fea39c927e58b2
Marialauramn16 <hikco25@gmail.com>
2021-05-04 23:21:54 +0200

    cambiando funcion y variables para colocar el boton inhabilitado, tambien arreglar el if y los espacios de cada miembro

--- js/main.js
+++ js/main.js
@@ -3,19 +3,20 @@ const resultext = document.getElementById("result");
 const group= document.getElementById("lista");
 const names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
+group.innerText = names.join(", ");
+
 function selectName() {
-    const position = Math.floor(Math.random() * names.length);
-    let winner = names[position];
-    resultext.innerHTML = winner;
-    names.splice(position, 1);
-    group.innerText = names;
-    if (names.length<0){
-        button.disabled=true;
+    if (names.length > 0) { // cuando la cantidad de elementos del objeto names sea mayor a 0
+        const position = Math.floor(Math.random() * names.length);
+        let winner = names[position];
+        resultext.innerHTML = winner;
+        names.splice(position, 1);
+        group.innerText = names.join(", ");
+    }
+    else {
+        document.getElementById("spin").disabled=true;
     }
 }
-
-
-
 button.addEventListener("click",selectName);
 
 
