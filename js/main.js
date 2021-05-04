8e846801df62065db4d776cb3dc7c428fc89e016
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-05-04 10:23:50 +0200

    lista group

--- js/main.js
+++ js/main.js
@@ -1,20 +1,16 @@
 const button = document.getElementById("spin");
 const resultext = document.getElementById("result");
-let group = document.getElementById("lista");
+const group= document.getElementById("lista");
 const names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
 function selectName(){
     let winner = names[Math.floor(Math.random() * names.length)];
     resultext.innerHTML = winner;
+    group.innerText = names;
     let indexOfWinner = names.indexOf(winner);
     let deleteName = names.splice(indexOfWinner,1);
 }
+button.addEventListener("click",selectName);
 
-function showList(){
-    let lista = names.slice();
-    group.innerHTML = lista;
-    let deleteName = names.splice(group,1);
-}
-button.addEventListener("click",selectName)
-names.addEventListener("load", showList)
+
 
