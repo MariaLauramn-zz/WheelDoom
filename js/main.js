4a4d1186f4fa7948bd5026157c57b60e4dda3045
superco-art <daniela.trifu@gmail.com>
2021-05-04 15:36:46 +0200

    actualizando boton spin

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-const button = document.getElementById("spin");
+let button = document.getElementById("spin");
 const resultext = document.getElementById("result");
 const group= document.getElementById("lista");
 const names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
 function selectName() {
-    let winner = names[Math.floor(Math.random() * names.length)];
+    const position = Math.floor(Math.random() * names.length);
+    let winner = names[position];
     resultext.innerHTML = winner;
+    names.splice(position, 1);
     group.innerText = names;
-    let indexOfWinner = names.indexOf(winner);
-    let deleteName = names.splice(indexOfWinner, 1);
-    if (names.length<0) {
-        button.enable = false;
-        return "Reload the game";
-                }
-    else {
-        button.enable = true;
-        }
+    if (names.length<0){
+        button.disabled=true;
+    }
 }
+
+
+
 button.addEventListener("click",selectName);
 
 
