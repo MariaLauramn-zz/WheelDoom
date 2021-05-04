d9c1324e720e8a2764eb68886c537f034ec76a43
superco-art <daniela.trifu@gmail.com>
2021-05-04 13:00:49 +0200

    reload the page

--- js/main.js
+++ js/main.js
@@ -3,14 +3,20 @@ const resultext = document.getElementById("result");
 const group= document.getElementById("lista");
 const names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
-function selectName(){
+function selectName() {
     let winner = names[Math.floor(Math.random() * names.length)];
     resultext.innerHTML = winner;
     group.innerText = names;
     let indexOfWinner = names.indexOf(winner);
-    let deleteName = names.splice(indexOfWinner,1);
+    let deleteName = names.splice(indexOfWinner, 1);
+    if (names.length<0) {
+        button.enable = false;
+        return "Reload the game";
+                }
+    else {
+        button.enable = true;
+        }
 }
-
 button.addEventListener("click",selectName);
 
 
